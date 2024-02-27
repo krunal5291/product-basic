3e0f145ef18ec541101f76ea9f32583e5ad4191b
krunal5291 <krunalkalathiya11@gmail.com>
2024-02-27 09:23:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let products = []
+
+const ui = () => {
+    document.getElementById("ui").innerHTML = ""
+    for (let i = 0; i <= products.length; i++) {
+        let h1 = document.createElement("h1")
+        h1 = products[i].titel
+        let img = document.createElement("img")
+        img.src = products[i].img
+        let price = document.createElement("h3")
+        price = products[i].price
+        let div = document.createElement("div")
+        div.append(img, h1, price)
+        document.getElementById("ui").append(div)
+    }
+}
+
+const header = (e) => {
+
+    e.preventDefault()
+
+    let titel = document.getElementById("titel").value
+    let img = document.getElementById("img").value
+    let price = document.getElementById("price").value
+
+    let prodduct = {
+        titel: titel,
+        img: img,
+        price: price
+    }
+    products.push(prodduct);
+    ui()
+    console.log(products);
+
+}
+
+
+
+document.getElementById("form").addEventListener("submit", header)
+
